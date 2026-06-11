13565c0b7f2891dca1c5feafb8629c7200f64f50
sayo-dev <akinsam2443@gmail.com>
2026-06-11 17:42:26 +0100

    initialize application

---
+++ src/main/java/com/teamzeu/velo/VeloApplication.java
@@ -0,0 +1,13 @@
+package com.teamzeu.velo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeloApplication.class, args);
+	}
+
+}
